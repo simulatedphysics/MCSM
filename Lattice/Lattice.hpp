9255764d2ecead1633e20198ee2ec9430f04342f
solidangle <obrienpja@gmail.com>
2018-12-25 14:21:19 -0500

    Define virtual constructor for Lattice class

--- Lattice/Lattice.hpp
+++ Lattice/Lattice.hpp
@@ -16,12 +16,12 @@ public:
 
     std::vector<Site> lat;
 
-    virtual void generate_lattice()=0;
+    virtual void generate_lattice() = 0;
     void print_lattice(){for(auto &m:lat){m.print_site();}};
 
     virtual void set_neighbors() = 0;
     virtual int convert_to_index(int, int) = 0;
 
 //    Probably should set this, because this is a base class
-//    virtual ~Lattice(){};
+    virtual ~Lattice() = default;;
 };
