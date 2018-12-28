f185a277175de884ee7bf59c90bbd62be94b77b0
solidangle <obrienpja@gmail.com>
2018-12-28 14:32:32 -0500

    Privatize neighbors and return reference to neighbors

--- Lattice/Site.hpp
+++ Lattice/Site.hpp
@@ -16,15 +16,15 @@ private:
     int y;
     int z;
     Spin spin;
+    std::vector<Site*> neighbors;
 
 public:
     Site(int in_x, int in_y, int in_z);
-    std::vector<Site*> neighbors;
     int getX(){return x;};
     int getY(){return y;};
     int getZ(){return z;};
     Spin & getSpin(){return spin;};
-
+    std::vector<Site*> & getNeighbors(){return neighbors;};
     void print_site(){std::cout << getX() << ", " << getY() << ", " << getZ() << std::endl;}
     void set_spin(Spin new_s){spin = new_s;}
 };
