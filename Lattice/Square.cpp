715cd5d3150b97b5935cc7a180bfaa6b2073ae00
solidangle <obrienpja@gmail.com>
2018-12-28 14:31:28 -0500

    Use emplace_back() in place of push_back()

--- Lattice/Square.cpp
+++ Lattice/Square.cpp
@@ -8,7 +8,7 @@
 void Square::generate_lattice() {
     for (int i = 0; i < 4; i++) {
         for(int j = 0; j < 4; j++) {
-            getLattice().push_back(Site(j, i, 0));
+            getLattice().emplace_back(Site(j, i, 0));
         }
     }
 }
@@ -19,11 +19,13 @@ int Square::convert_to_index(int x_, int y_) {
 
 void Square::set_neighbors() {
     std::vector<Site>::pointer a;
-    a = &getLattice().front();
+    a = &(getLattice().front());
     for(int i = 0; i < get_nx(); i++)
-        for (int j =0; j< get_ny(); j++){
+        for (int j =0; j < get_ny(); j++){
             int site_ind(convert_to_index(i , j));
-            getLattice()[site_ind].neighbors.push_back(a + convert_to_index(i % get_nx(), (j + 1) % get_ny()));
-            getLattice()[site_ind].neighbors.push_back(a + convert_to_index((i + 1) % get_nx(), j % get_ny()));
+            getLattice()[site_ind].getNeighbors().push_back(a + convert_to_index(i % get_nx(), (j + 1) % get_ny()));
+            getLattice()[site_ind].getNeighbors().push_back(a + convert_to_index((i + 1) % get_nx(), j % get_ny()));
+            getLattice()[site_ind].getNeighbors().push_back(a + convert_to_index(i % get_nx(), (j - 1 + get_ny()) % get_ny()));
+            getLattice()[site_ind].getNeighbors().push_back(a + convert_to_index((i - 1 + get_nx()) % get_nx(), j % get_ny()));
         }
 }
