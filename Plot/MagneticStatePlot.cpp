699ce19fcca6cae50050e0bcc5024c729d08377d
solidangle <obrienpja@gmail.com>
2019-01-10 23:36:28 -0500

    Adds pdf of magnetic state

--- Plot/MagneticStatePlot.cpp
+++ Plot/MagneticStatePlot.cpp
@@ -30,7 +30,8 @@ void demo_basic(std::unique_ptr<Model> & model_ptr) {
     gp << "unset tics\n";
     gp << "unset border\n";
     gp << "set size sq\n";
-    gp << "set term x11\n";
+    gp << "set output 'ising.png'\n";
+//    gp << "set term x11\n";
     gp << "set style line 1 lt 1 lc rgb \"black\" lw 2\n";
     gp << "set style fill solid 2.0 noborder\n";
     gp << "set title \"Ising Model: " << nx << " x " << ny << " Unit Cells\"\n";
@@ -59,5 +60,6 @@ void demo_basic(std::unique_ptr<Model> & model_ptr) {
 
     gp << "set sample 5000\n";
     gp << "p -4 ls 1 notitle\n";
-    gp << "set output 'ising.png'\n";
+
+//    gp << "replot\n";
 }
