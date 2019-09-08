bf43a6574703edc8c62f0720c97406731df2d39c
solidangle <obrienpja@gmail.com>
2019-09-08 15:14:11 -0500

    Add primitive vectors a1, a2, and a3 to Lattice class

--- Lattice/Lattice.hpp
+++ Lattice/Lattice.hpp
@@ -12,6 +12,7 @@ class Lattice{
     int n_x;
     int n_y;
     int n_z = 0;
+    vec3 a1, a2, a3;
     std::vector<Site> lat;
 public:
     virtual void generate_lattice() = 0;
@@ -30,6 +31,14 @@ public:
     void set_nx(int nx_in){n_x = nx_in;};
     void set_ny(int ny_in){n_y = ny_in;};
     void set_nz(int nz_in){n_z = nz_in;};
+
+    void set_a1(vec3 a1_in){a1 = a1_in;};
+    void set_a2(vec3 a2_in){a2 = a2_in;};
+    void set_a3(vec3 a3_in){a3 = a3_in;};
+
+    vec3 get_a1(){return a1;};
+    vec3 get_a2(){return a2;};
+    vec3 get_a3(){return a3;};
 
     virtual ~Lattice() = default;;
 };
